e684981afb2c44b2d6d5454dedf89c51613840e8
karlitoyo <karltimmins@gmail.com>
2025-01-02 00:09:21 +0000

    Updated venueDashboard.tsx to fix jwt error being shown related to mis-match of key, issue resolved. Next steps further styling of Veneu dashboard page and calendar booking requires start.

--- frontend/pages/venueDashboard.tsx
+++ frontend/pages/venueDashboard.tsx
@@ -1,9 +1,22 @@
-import { GetServerSideProps } from 'next';
-import VenueDashboardPage from '../components/venueDashboard/VenueDashboardPage';
-import nookies from 'nookies';
-import { JwtService } from '@nestjs/jwt';
+import { GetServerSideProps } from "next";
 
-const Dashboard = ({ userId, token }: { userId: string; token: string }) => {
+import VenueDashboardPage from "../components/venueDashboard/VenueDashboardPage";
+
+import nookies from "nookies";
+
+import { JwtService } from "@nestjs/jwt";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const Dashboard = ({
+  userId,
+  token,
+}: {
+  userId: string | null;
+  token: string;
+}) => {
   return <VenueDashboardPage userId={userId} token={token} />;
 };
 
@@ -11,32 +24,46 @@ const jwtService = new JwtService();
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const cookies = nookies.get(context);
+
   const token = cookies.token;
 
   if (!token) {
     console.log("No token found in cookies");
+
     return {
       redirect: {
-        destination: '/login',
+        destination: "/login",
+
         permanent: false,
       },
     };
   }
 
   try {
-    const decodedToken: any = jwtService.verify(token, { secret: process.env.SECRET_KEY! }); // Replace with your backend JWT secret
+    const secret = process.env.SECRET_KEY;
+
+    if (!secret) {
+      throw new Error("Secret key is not defined");
+    }
+
+    const decodedToken: any = jwtService.verify(token, { secret });
+
+    const userId = decodedToken.sub || decodedToken.id || null; // Ensure userId is extracted correctly
+
     return {
       props: {
-        userId: decodedToken.id, // Adjust based on your JWT payload
+        userId,
         token,
       },
     };
   } catch (error) {
-    console.log("redirecting to login due to error:", error);
+    console.log("Redirecting to login due to error:", error);
+
     return {
-      props: {
-        token,
-        // Add other decoded properties as needed
+      redirect: {
+        destination: "/venueLogin",
+
+        permanent: false,
       },
     };
   }
