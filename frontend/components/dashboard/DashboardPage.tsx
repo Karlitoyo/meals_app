b5523124e376e1bc03b5881c9b93d5db2264fa80
karlitoyo <karltimmins@gmail.com>
2025-01-25 20:00:34 +0000

    updated Dashboard component to take in props from card component, token and values now being passed.

--- frontend/components/dashboard/DashboardPage.tsx
+++ frontend/components/dashboard/DashboardPage.tsx
@@ -48,7 +48,7 @@ const DashboardPage: React.FC<DashboardPageProps> = ({ userId, token }) => {
         <h1 className="text-3xl font-bold text-center mb-8 mt-8">Global Meals</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {services.map((service: Service) => (
-            <Card
+            <Card token={token} venueId={userId}
               key={service.id}
               service={service}
               onSelect={() => handleCardClick(service.id)}
